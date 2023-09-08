adf494d2ae18c977f480e4e326934afdff6316d0
gitychub <yc.huang@mail.utoronto.ca>
2023-09-08 16:21:02 +0800

    [pte printout] passed

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,23 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static char *prefixes[4] = { "", " ..", " .. ..", " .. .. .." };
+
+void vmwalk(pagetable_t pagetable, int depth) {
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      uint64 child = PTE2PA(pte);
+      printf("%s%d: pte %p pa %p\n", prefixes[depth], i, pte, child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        vmwalk((pagetable_t)child, depth + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmwalk(pagetable, 1);
+}
